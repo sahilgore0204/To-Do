39ab329b11543fbf19a64fb49365847088e49add
Sahil <sahilgore1998@gmail.com>
2023-01-08 18:33:49 +0530

    first commit: created input form

---
+++ src/Components/AddTask.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '../App.css';
+
+export default function AddTask(){
+    return <div className="add-task-container">
+        <h1>Add a task</h1>
+        <form>
+            <label for="title" >Title</label> <br/>
+            <input id="title" type='text' name="title" maxLength="100" placeholder="Title of the task" required /> <br/>
+
+            <label for="description">Description</label> <br/>
+            <input id="description" type='text' name="description" maxLength="1000" placeholder="Describe the task" required /> <br/>
+
+            <label for="duedate">Due Date</label> <br/>
+            <input id="duedate" type='date' name="duedate"/> <br/>
+
+            <label for="tags">Tags</label> <br/>
+            <input id="tags" type="text" name="tags" placeholder="Add tags"/> <br/>
+
+            <label for="status">Status:</label> <br/>
+            <select name="status" id="status" required>
+                <option value="OPEN" selected>OPEN</option>
+                <option value="WORKING">WORKING</option>
+                <option value="DONE">DONE</option>
+                <option value="OVERDUE">OVERDUE</option>
+            </select>
+        </form>
+    </div>
+}
