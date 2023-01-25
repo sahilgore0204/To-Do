e35f1a77a850b44b6feefb238cb4b46ad93b0a28
Sahil <sahilgore1998@gmail.com>
2023-01-25 23:59:32 +0530

    fifth commit: for for taking task input complete

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -2,29 +2,45 @@ import React,{useState} from "react";
 import '../App.css';
 import { useVisiblity,useToggle } from "../Contexts/VisiblityContext";
 import Button from "./Button";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import '../App.css';
 export default function AddTask(props){
     const visible=useVisiblity();
     console.log("addTask");
     const toggle=useToggle();
-    const [tags,setTags]=useState([]);
+    const [taskInfo,setTaskInfo]=useState({title:"",description:"",tags:"",status:"OPEN"});
+    const [startDate, setStartDate] = useState(new Date());
+
+    function handleChange(event){
+        setTaskInfo({...taskInfo,[event.target.name]:event.target.value});
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(taskInfo);
+        resetTaskInfo();
+    }
+    function resetTaskInfo(){
+        setTaskInfo({title:"",description:"",tags:"",status:"OPEN"});
+    }
     return <div className="add-task-container">
         <div className="add-task-modal" style={{display:visible?"block":"none"}}>
-         <Button type="customize" duty="close-modal">&times;</Button>
-         <form>
+         <Button reset={resetTaskInfo} type="customize" duty="close-modal">&times;</Button>
+         <form onSubmit={handleSubmit}>
             <label for="title" >Title</label> <br/>
-            <input id="title" type='text' name="title" maxLength="100" placeholder="Title of the task" required /> <br/>
+            <input onChange={handleChange} id="title" type='text' name="title" maxLength="100" placeholder="Title of the task" required  value={taskInfo.title}/> <br/>
 
             <label for="description">Description</label> <br/>
-            <input id="description" type='text' name="description" maxLength="1000" placeholder="Describe the task" required /> <br/>
+            <input onChange={handleChange} id="description" type='text' name="description" maxLength="1000" placeholder="Describe the task" required value={taskInfo.description} /> <br/>
 
             <label for="duedate">Due Date</label> <br/>
-            <input id="duedate" type='date' name="duedate"/> <br/>
+            <DatePicker minDate={new Date()} selected={startDate} onChange={(date) => setStartDate(date)} /> <br/>
 
             <label for="tags">Tags</label> <br/>
-            <input id="tags" type="text" name="tags" placeholder="Add tags(comma separated)"/> <br/>
+            <input onChange={handleChange} id="tags" type="text" name="tags" placeholder="Add tags(comma separated)" value={taskInfo.tags} /> <br/>
 
             <label for="status">Status:</label> <br/>
-            <select name="status" id="status" required>
+            <select onChange={handleChange} name="status" id="status" required value={taskInfo.status}>
                 <option value="OPEN" selected>OPEN</option>
                 <option value="WORKING">WORKING</option>
                 <option value="DONE">DONE</option>
@@ -33,7 +49,10 @@ export default function AddTask(props){
             <Button type="medium-btn" duty="submit">Submit</Button>
          </form>
         </div>
-        <div onClick={toggle} className="add-task-modal-overlay" style={{display:visible?"block":"none",pointerEvents:visible?"all":"none"}}>
+        <div onClick={()=>{
+            resetTaskInfo();
+            toggle();
+        }} className="add-task-modal-overlay" style={{display:visible?"block":"none",pointerEvents:visible?"all":"none"}}>
 
         </div>
     </div>
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,12 +5,14 @@ export default function Button(props){
     let visiblity=useToggle();
     //console.log("button");
     function handleClick(){
-        if(props.duty==="add-task")
-        visiblity();
+        if(props.duty==="add-task"){
+            visiblity();
+        }
         else if(props.duty==="submit"){
             console.log("form submitted");
         }
         else if(props.duty==="close-modal"){
+            props.reset();
             visiblity();
         }
     }
